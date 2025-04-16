f7aefe4033ba573f0bf877ceba1e2742c9a4ca81
developerTamanna <astamanna57@gmail.com>
2025-04-16 21:55:29 +0600

    submitForm event target

---
+++ src/components/SubmitFrom/SubmitFrom.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+const SubmitFrom = () => {
+    //name function    
+  const [text, setText] = useState('');
+//   console.log(text)
+const [email, setEmail] = useState('');
+console.log(email)
+
+    const submitForm =(event)=>{
+        event.preventDefault()
+        console.log(event.target.text.value)
+        console.log(event.target.email.value)
+        console.log(event.target.password.value)
+    }
+
+
+    const nameText =(event)=>{
+       setText(event.target.value)
+    }
+
+
+    //email function
+
+    const emailValue = (event)=>{
+        setEmail(event.target.value)
+    }
+
+    return (
+<div className='border p-2 text-center bg-red-50 rounded-md'>
+
+<form onSubmit={submitForm} >
+<input  onChange={nameText}
+  type="text" name='text'
+  placeholder="Type your name..." 
+  className="border border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+/>
+
+
+<br/>
+<br/>
+
+<input onChange={emailValue} className='border border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 ' type="email" name="email" placeholder='type your email' />
+<br/>
+<br/>
+<input className='border border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' type="password" name="password" />
+
+<br/>
+<br/>
+
+<input className='border border-blue-400 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' type="submit" value="Submit"/>
+
+</form>
+        </div>
+    );
+};
+
+export default SubmitFrom;
